566bb5e1b755592b6dcf804d8c93a49a8d3b1637
CENTRAL\sberteloot <sberteloot@coto.com.ar>
2023-06-20 16:04:41 -0300

    Commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './componentes/main/main.component';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+import { ToolbarComponent } from './componentes/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    NavbarComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule
